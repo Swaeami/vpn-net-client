b8ae4a317d266d26612a35f6b70f72f5c0df39a6
Swaeami <swaeami@gmail.com>
2025-06-21 01:42:45 +0100

    test

--- helpers/admin/windows.go
+++ helpers/admin/windows.go
@@ -3,33 +3,25 @@
 package admin
 
 import (
-	"golang.org/x/sys/windows"
+	"syscall"
 )
 
 func IsAdmin() (bool, error) {
-	return isRunningAsAdmin()
+	isAdmin, err := isRunningAsAdmin()
+	if err != nil {
+		return false, err
+	}
+
+	if !isAdmin {
+		return false, nil
+	}
+	return true, nil
 }
 
 func isRunningAsAdmin() (bool, error) {
-	var sid *windows.SID
+	shell32 := syscall.NewLazyDLL("shell32.dll")
+	isUserAnAdmin := shell32.NewProc("IsUserAnAdmin")
 
-	err := windows.AllocateAndInitializeSid(
-		&windows.SECURITY_NT_AUTHORITY,
-		2,
-		windows.SECURITY_BUILTIN_DOMAIN_RID,
-		windows.DOMAIN_ALIAS_RID_ADMINS,
-		0, 0, 0, 0, 0, 0,
-		&sid)
-	if err != nil {
-		return false, err
-	}
-	defer windows.FreeSid(sid)
-
-	token := windows.Token(0)
-	member, err := token.IsMember(sid)
-	if err != nil {
-		return false, err
-	}
-
-	return member, nil
+	ret, _, _ := isUserAnAdmin.Call()
+	return ret != 0, nil
 }
